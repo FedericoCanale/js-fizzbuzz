2b299cd4af9e3dfcabf0693fbc90872729981241
FedericoCanale <icocanale@gmail.com>
2025-10-07 14:01:15 +0200

    add fizz to multiple of 3 and buzz to multiple of 5

--- assets/main.js
+++ assets/main.js
@@ -7,7 +7,14 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 // start by creating a loop that goes from 1 to 100
 for (let i = 1; i <= 100; i++) {
     //console.log(i); checking if the loop works
+if (i % 3 === 0 ) {
+        console.log("Fizz");
+}else if (i % 5 === 0 ){
+        console.log("Buzz");
+}/*else {
+    console.log(i) check when a number is a multiply of 5 or 3
+}
+ */       
 
-    
     
 }
